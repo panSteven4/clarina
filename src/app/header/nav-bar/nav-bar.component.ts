9f9e25359552a5292ad4bbc000823c8cdc3b23ce
Štěpán Omelka <stepomelka@gmail.com>
2023-07-28 10:48:41 +0200

    hover nav-item, hover shopping cart effects added

--- src/app/header/nav-bar/nav-bar.component.ts
+++ src/app/header/nav-bar/nav-bar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  clicked: boolean = false;
 
+  dropItDown() {
+    this.clicked = !this.clicked;
+  }
 }
