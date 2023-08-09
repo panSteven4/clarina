21cffa5f3d42b7f981d09482b37e74694342a2cc
Štěpán Omelka <stepomelka@gmail.com>
2023-08-09 19:37:43 +0200

    registration component styling

--- src/app/views/registration/registration.component.ts
+++ src/app/views/registration/registration.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  isDroppedFirm = false;
+  isDroppedAddress = false;
+  toggleFirm(){
+    this.isDroppedFirm = !this.isDroppedFirm;
+  }
+
+  toggleAddress() {
+    this.isDroppedAddress = !this.isDroppedAddress;
+  }
 
 }
