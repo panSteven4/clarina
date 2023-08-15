e998d59273f4447e271720a32b86239e5fa5a451
Štěpán Omelka <stepomelka@gmail.com>
2023-08-15 18:15:32 +0200

    when product card hover, changes styles of elements inside it

--- src/app/content/product-list/product/product.component.ts
+++ src/app/content/product-list/product/product.component.ts
@@ -11,37 +11,17 @@ export class ProductComponent {
   readonly ProductAvailabilityEnum = ProductAvailabilityEnum;
 
   @Input() product: ProductModel | undefined;
+
+  isHovered = false;
   constructor() {
-  //   z vysvětlování od sama - checklist
-  //   if (!this.product) {
-  //     return;
-  //   }
-  //   const {price, warranty} = this.product;
-  //   const foo = price + 'kc';
-  //   const foo2 = `cena je ${price * 100}kc`;
-  //
-  //   const price2 = 20;
-  //
-  //   const neco = {
-  //     ...this.product,
-  //       price,
-  //   }
-  //
-  //   const temp = false;
-  //
-  //   temp && this.funkce(this.product);
-  //
-  //   if (temp) {
-  //     this.funkce2()
-  //   }
-  //
-  // }
-  //
-  //   funkce({ price }: ProductModel): void {
-  //     console.log(price);
-  //   }
-  //
-  //   funkce2(price?: number) {
-  //
+
   }
+  onHover() {
+    this.isHovered = true;
+  }
+  onHoverEnd() {
+    this.isHovered = false;
+  }
+
+  protected readonly onmouseout = onmouseout;
 }
